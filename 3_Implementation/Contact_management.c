a77b0898e073075d75d6f39431107d0dd4d36869
Shivangi <41469660+SHIVANGITHAKUR@users.noreply.github.com>
2021-04-17 19:33:06 +0530

    Update Contact_management.c

--- 3_Implementation/Contact_management.c
+++ 3_Implementation/Contact_management.c
@@ -12,11 +12,11 @@ char phone_number[20];
 int main(){
     int choice;
     printf("\n\n\t----------------------CONTACT MANAGEMENT SYSTEM------------------------");
-    printf("\n\t[1] Add Contact");
-    printf("\n\t[2] View Contact");
+    printf("\n\t[1] Add Contact ");
+    printf("\n\t[2] View Contact ");
     printf("\n\t[3] Help");
     printf("\n\t[4] Exit");
-    printf("\n\tEnter your choice");
+    printf("\n\tEnter your choice : ");
     scanf("%d", &choice);
     switch (choice)
     {
